151738e66609632db824e7df3d64be645e1683f1
SumayaMahabub <168766832+SumayaMahabub@users.noreply.github.com>
2024-05-04 17:35:48 +0400

    Update TrainingRecord.java

--- src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
+++ src/main/java/com/stir/cscu9t4practical1/TrainingRecord.java
@@ -1,43 +1,106 @@
 // An implementation of a Training Record as an ArrayList
 package com.stir.cscu9t4practical1;
 
-
-
-
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class TrainingRecord {
     private List<Entry> tr;
-    
+
     public TrainingRecord() {
         tr = new ArrayList<Entry>();
     } //constructor
-    
+
     // add a record to the list
-   public void addEntry(Entry e){
-       tr.add(e);    
-   } // addClass
-   
-   // look up the entry of a given day and month
-   public String lookupEntry (int d, int m, int y) {
-       ListIterator<Entry> iter = tr.listIterator();
-       String result = "No entries found";
-       while (iter.hasNext()) {
-          Entry current = iter.next();
-          if (current.getDay()==d && current.getMonth()==m && current.getYear()==y) 
-             result = current.getEntry();
+    public String addEntry(Entry e)
+    {
+        String message = "Record has been added\n";
+        String name = e.getName();
+        int day = e.getDay();
+        int month = e.getMonth();
+        int year = e.getYear();
+        boolean record = false;
+
+        ListIterator<Entry> iter = tr.listIterator();
+        while (iter.hasNext())
+        {
+            Entry current = iter.next();
+            if(current.getName().equalsIgnoreCase(name) && current.getDay()==day && current.getMonth()==month && current.getYear()==year)
+            {
+                message = "Record already exists.\n";
+                record=true;
+                break;
             }
-       return result;
-   } // lookupEntry
-   
-   // Count the number of entries
-   public int getNumberOfEntries(){
-       return tr.size();
-   }
-   // Clear all entries
-   public void clearAllEntries(){
-       tr.clear();
-   }
-   
+
+        }
+
+        if (!record) {
+            tr.add(e);
+        }
+
+        return message;
+
+    } // addClass
+
+    public String deleteEntry(String name, int day, int month, int year)
+    {
+        String message = "Record not found\n";
+
+        ListIterator<Entry> iter = tr.listIterator();
+        while (iter.hasNext())
+        {
+            Entry current = iter.next();
+            if(current.getName().equalsIgnoreCase(name) && current.getDay()==day && current.getMonth()==month && current.getYear()==year)
+            {
+                message = "Record has been deleted.\n";
+                tr.remove(current);
+                break;
+            }
+
+        }
+
+
+        return message;
+    }
+
+    // look up the entry of a given day and month
+    public String lookupEntry (int d, int m, int y) {
+        ListIterator<Entry> iter = tr.listIterator();
+        String result = "No entries found";
+        while (iter.hasNext()) {
+            Entry current = iter.next();
+            if (current.getDay()==d && current.getMonth()==m && current.getYear()==y)
+                result = current.getEntry();
+        }
+        return result;
+    } // lookupEntry
+
+    public String lookupEntries(int d, int m, int y)
+    {
+
+        ListIterator<Entry> iter = tr.listIterator();
+        String result = "";
+        while (iter.hasNext()) {
+            Entry current = iter.next();
+            if (current.getDay()==d && current.getMonth()==m && current.getYear()==y)
+                result += current.getEntry();
+        }
+        if(result.isBlank())
+        {
+            result="No records found for this date";
+        }
+        return result;
+
+    }
+
+    // Count the number of entries
+    public int getNumberOfEntries(){
+        return tr.size();
+    }
+    // Clear all entries
+    public void clearAllEntries(){
+        tr.clear();
+    }
+
 } // TrainingRecord
